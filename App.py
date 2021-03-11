552eb997046085538715e3d5d1cefa6a66016ad3
bsh <dan990429@gmail.com>
2021-03-11 15:33:50 +0900

    a

--- App.py
+++ App.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template("index.html")
 
+
+@app.route("/app", methods=["POST"])
+def bpp():
+    print(request.get_json())
+    return jsonify(request.get_json())
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run()
